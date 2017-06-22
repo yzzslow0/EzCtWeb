9084c35cbc7294d56ab2001b3310a8d16b4b145c
yzz <fzlhzd@gmail.com>
2017-06-22 16:54:27 +0800

    update

--- src/main/java/com/yzz/ct/controller/UserController.java
+++ src/main/java/com/yzz/ct/controller/UserController.java
@@ -1,6 +1,10 @@
 package com.yzz.ct.controller;
 
 import com.jfinal.core.Controller;
+import com.yzz.ct.common.model.User;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by yzz on 2017/6/22.
@@ -13,6 +17,19 @@ public class UserController extends Controller {
 
 
     public void register(){
-        renderText("zhuce");
+
+       User user = new User();
+       user.setId(null);
+       user.setPassword("123");
+       user.setName("呵呵哒");
+       user.save();
+
+    }
+
+    public void userList(){
+
+        List<User> userList = User.dao.find("select * FROM `user`");
+        renderText(userList+"");
+
     }
 }
---
+++ src/main/java/com/yzz/ct/service/UserService.java
@@ -0,0 +1,13 @@
+package com.yzz.ct.service;
+
+import com.yzz.ct.common.model.User;
+
+/**
+ * Created by yzz on 2017/6/22.
+ */
+public class UserService {
+
+    public void register(){
+
+    }
+}
