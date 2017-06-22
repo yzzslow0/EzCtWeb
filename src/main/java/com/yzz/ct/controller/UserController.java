e84f42de7dda5265630f3f05bafe9524359105c4
yzz <fzlhzd@gmail.com>
2017-06-22 16:13:01 +0800

    Initial commit

---
+++ src/main/java/com/yzz/ct/controller/UserController.java
@@ -0,0 +1,18 @@
+package com.yzz.ct.controller;
+
+import com.jfinal.core.Controller;
+
+/**
+ * Created by yzz on 2017/6/22.
+ */
+public class UserController extends Controller {
+
+    public void index() {
+       renderText("我不不不不不不不是浪费大家酸辣粉");
+    }
+
+
+    public void register(){
+        renderText("zhuce");
+    }
+}
